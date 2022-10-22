e0f65d9b59e1ee32a4689a6a9cb324b2c254f7ee
chika918 <113702101+chika918@users.noreply.github.com>
2022-10-23 06:33:07 +0900

    JavaScript1章のチェックテスト提出

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ isEven(13);
 isEven(15);
 isEven(18);
 isEven(22);
+
+//問2:以下の要件を満たすように実装してください。
+/*
+【要件】
+・インスタンス化した時にガソリンとナンバーがセットされるようなCarクラスを作成する
+・「ガソリンは〇〇です。ナンバーは△△です」と出力される「getNumGas」という関数を作成する
+*/
+class Car{
+    constructor(gas, num){
+        this.gas = gas;
+        this.num = num
+    }
+    getNumGas(){
+        console.log(`ガソリンは${this.gas}です。ナンバーは${this.num}です`)
+    }
+}
+let car1 = new Car('〇〇' , '△△');
+car1.getNumGas();
